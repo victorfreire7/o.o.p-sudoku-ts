eaf17fd4043bcb47c70f782e9bb217185e63a14b
victorfreire7 <vhugo.freire159@gmail.com>
2026-02-17 17:59:20 -0300

    add getSudoku() and getGame() method

--- class/Sudoku.ts
+++ class/Sudoku.ts
@@ -2,26 +2,40 @@
 // O PARAMETRO ENVIADO É O PROMPT ENVIADO PELO USUÁRIO.
 // ELA TAMBÉM VERIFICA SE AS LINHAS E COLUNAS ESTAO CORRETAS.
 
-import Block from './Block';
+import Block from './Block.ts';
 
 export default class Sudoku{
     private block: Block = new Block(); // vou usar essa instancia para criar os blocks com block.createBlock();
     private static sudoku: Sudoku | null = null; // explicação em 'getSudoku()'
-
-    private constructor(
-        private prompt?: string // nao torno obrigatorio pois na primeira (e unica) instancia do sudoku eu nao envio um prompt.
-    ) {}
+    private game: Array<Array<Array<number>>> = [];
 
     setPromptInSudoku(): void {
         // aqui eu vou alterar o sudoku de acordo com o prompt
         // e depois executar getSudoku(); para retorna-lo
     }
 
-    static getSudoku(prompt: string | null = null): Sudoku { // to fazendo isso pra nao permitir uma instancia externa e nem uma segunda instancia
+
+
+    static getGame(): Array<Array<Array<number>>> | null {
+        return Sudoku.sudoku.game;
+    }
+
+    static setGame(): void {
+        
+    }
+
+    authGame(): void {
+        
+    }
+
+    static getSudoku(): Sudoku { // to fazendo isso pra nao permitir uma instancia externa e nem uma segunda instancia
         if(Sudoku.sudoku) {
             return Sudoku.sudoku;
         }
-        Sudoku.sudoku = new Sudoku(prompt);
+        Sudoku.sudoku = new Sudoku();
+        for(var i = 0; i < 6; i++){
+            Sudoku.sudoku.game.push(Sudoku.sudoku.block.createBlock());
+        }
         return Sudoku.sudoku;
     }
 }
