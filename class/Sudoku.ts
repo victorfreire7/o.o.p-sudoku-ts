510884acd7f625791b1e131b41541fd9947f3c83
victorfreire7 <vhugo.freire159@gmail.com>
2026-02-17 01:07:13 -0300

    set null union type

--- class/Sudoku.ts
+++ class/Sudoku.ts
@@ -6,7 +6,7 @@ import Block from './Block';
 
 export default class Sudoku{
     private block: Block = new Block(); // vou usar essa instancia para criar os blocks com block.createBlock();
-    private static sudoku: Sudoku; // explicação em 'getSudoku()'
+    private static sudoku: Sudoku | null = null; // explicação em 'getSudoku()'
 
     private constructor(
         private prompt?: string // nao torno obrigatorio pois na primeira (e unica) instancia do sudoku eu nao envio um prompt.
@@ -17,11 +17,11 @@ export default class Sudoku{
         // e depois executar getSudoku(); para retorna-lo
     }
 
-    static getSudoku(): Sudoku { // to fazendo isso pra nao permitir uma instancia externa e nem uma segunda instancia
+    static getSudoku(prompt: string | null = null): Sudoku { // to fazendo isso pra nao permitir uma instancia externa e nem uma segunda instancia
         if(Sudoku.sudoku) {
             return Sudoku.sudoku;
         }
-        Sudoku.sudoku = new Sudoku();
+        Sudoku.sudoku = new Sudoku(prompt);
         return Sudoku.sudoku;
     }
 }
